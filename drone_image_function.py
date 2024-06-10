cf907c0e861e5ab9b509118971a7469eafb6ec4f
Pedr0517 <pedroramos171st@gmail.com>
2024-06-10 13:47:35 +0200

    drone image change

--- drone_image_function.py
+++ drone_image_function.py
@@ -1,3 +1,4 @@
+import os
 import rclpy
 from rclpy.node import Node
 
@@ -7,17 +8,19 @@ from sensor_msgs.msg import Image
 class DroneImage(Node):
     def __init__(self):
         super().__init__('drone_image')
-        self.publisher = self.create_publisher(Image, 'topic', 1)
-        timer_period = 1.0
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.images = []
+        self.publisher = self.create_publisher(Image, 'charruco_image', 10)
+        self.i = 0
 
-    def timer_callback(self):
+    def image_upload(self):
         msg = Image()
         msg.data = 'image here'
+        self.folder_dir = "/project_cf_cam_calibration/drone_images"
+        self.image_name = (f'image{self.i}.png')
+        with open(os.path.join(self.folder_dir, self.image_name), 'wb') as f:
+            f.write(msg.data)
         self.publisher.publish(msg)
         self.get_logger().info('Image uploaded')
-        self.images.append(msg.data)
+        self.i += 1
 
 
 def main(args=None):
