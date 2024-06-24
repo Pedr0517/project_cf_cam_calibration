418ea149bbd21fe93c34975c8f5e3fd2484b6b43
Pedr0517 <pedroramos171st@gmail.com>
2024-06-24 09:48:35 +0200

    Folder clean up

--- check.py
+++
@@ -1,7 +0,0 @@
-
-
-for i in range(3):
-    if i == 0 or i == 2:
-        print('hi')
-    if i == 1:
-        print('hello')
--- path_plot.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-x = np.linspace(12, 1, 4)
-
-y = x[::-1]
-print(y)
